98861372dac82632e0cfb4ab4cf4471ea080182c
Chayakorn <jing182544@gmail.com>
2024-02-18 00:47:54 +0700

    [+] fix some bug profile tab

--- src/components/common/ProfileTab/type.ts
+++ src/components/common/ProfileTab/type.ts
@@ -1,8 +1,8 @@
 import { CSSProperties, ReactNode } from 'react';
 
 export type TProfileTab = {
-  profilePic: string;
-  userName: string;
+  profilePic?: string;
+  userName?: string;
   onClick: () => void;
   style?: CSSProperties;
 };
