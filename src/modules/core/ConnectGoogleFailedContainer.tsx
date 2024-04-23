3ed74e5be1a68c0f5face0707d07ec91abd023dd
Chayakorn <jing182544@gmail.com>
2024-04-23 15:26:36 +0700

    [+] add page connect account failed

---
+++ src/modules/core/ConnectGoogleFailedContainer.tsx
@@ -0,0 +1,32 @@
+import { modalAlert } from '@/components/common/Modal';
+import ErrorModal from '@/components/common/Modal/ModalError';
+import { useAppDispatch } from '@/hooks';
+import { useRouter } from 'next/router';
+import { ReactNode, useEffect } from 'react';
+export const ConnectGoogleFailedContainer = ({
+  render
+}: {
+  render: () => ReactNode;
+}) => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  useEffect(() => {
+    const modal = modalAlert();
+    modal.render({
+      closeable: false,
+      children: ErrorModal({
+        errorMessage: 'Your email already connect to the game.',
+        errorStatus: `Can't connect account.`
+      })
+    });
+    setTimeout(() => {
+      modal.destroy();
+      router.push('/');
+    }, 3000);
+  }, []);
+
+  return render();
+};
+
+export default ConnectGoogleFailedContainer;
